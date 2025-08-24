8eb4aef08d87355feabb829cedb5c2dc143bfed0
codr-shiv <codr.shiv@gmail.com>
2025-08-24 19:19:13 +0530

    Added README

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-  window.addEventListener("load", () => {
-    const loader = document.getElementById("loader");
-    const content = document.getElementById("content");
-    setTimeout(() => {
-        loader.classList.add("hidden");
-      content.style.display = "block";
-    }, 2000);
-  });
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  const content = document.getElementById("content");
+  setTimeout(() => {
+    loader.classList.add("hidden");
+    content.style.display = "block";
+  }, 2000);
+});
